7a977f17dabbdbde9076acf68162d495add4380d
Anna Langner <annalangner06@gmail.com>
2019-08-13 12:27:32 +0200

    Adding SRV adress

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://AnnaLangner:<Orzeszki2!>@cluster0-rjkr0.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
